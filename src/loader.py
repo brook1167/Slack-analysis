8eac82b473e5206bfcf39b8e8d0533a7bd3930ea
Biruk <birukbizuayehu1167@gmail.com>
2023-11-28 10:29:33 +0300

    fixing the dataframe

--- src/loader.py
+++ src/loader.py
@@ -53,7 +53,7 @@ class SlackDataLoader:
         combined = []
         for json_file in glob.glob(f"{path_channel}*.json"):
             with open(json_file, 'r', encoding="utf8") as slack_data:
-                combined.append(slack_data)
+                combined.append(json.load(slack_data))
 
         # loop through all json files and extract required informations
         dflist = []
